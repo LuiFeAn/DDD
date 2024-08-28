4a5dd71d6d33e650c1f07254f9531492887c907b
LuiFeAn <luisfernandogvv@gmail.com>
2024-08-28 20:18:55 -0300

    test: customer tests

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,47 @@
+import Adress from "./adress";
+import Customer from "./customer";
+
+describe("Customer unit tests", () => {
+  it("Should throw error when id is empty", () => {
+    expect(() => {
+      let customer = new Customer("", "Luis");
+    }).toThrow("Id is required");
+  });
+
+  it("Should throw error when name is empty", () => {
+    expect(() => {
+      let customer = new Customer("Luis", "");
+    }).toThrow("Name is required");
+  });
+
+  it("Should change name", () => {
+    //Arrange
+    let customer = new Customer("123", "Luis");
+
+    //Act
+    customer.changeName("Luis Fernando");
+
+    //Assert
+    expect(customer.name).toBe("Luis Fernando");
+  });
+
+  it("Should throw error when adress is undefined when you activate a customer", () => {
+    expect(() => {
+      let customer = new Customer("123", "Luis");
+      customer.activate();
+    }).toThrow("Adress is mandatory to activate customer");
+  });
+
+  it("Should activate customer", () => {
+    //Arrange
+    let customer = new Customer("123", "Luis");
+    const adress = new Adress("Rua Três", 2, "1234-5678", "Belém-PA");
+    customer.Adress = adress;
+
+    //Act
+    customer.activate();
+
+    //Assert
+    expect(customer.isActive).toBe(true);
+  });
+});
