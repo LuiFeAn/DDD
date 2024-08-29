5c3b3e5a4ed369c8d6ccb962b6cf2174e6175da1
LuiFeAn <luisfernandogvv@gmail.com>
2024-08-29 20:33:08 -0300

    test: adjust order tests

--- src/entity/order.spec.test.ts
+++ src/entity/order.spec.test.ts
@@ -3,7 +3,7 @@ import OrderItem from "./order_item";
 describe("Order unit tests", () => {
   it("Should throw error when id is empty", () => {
     expect(() => {
-      let order = new Order("", "123", []);
+      new Order("", "123", []);
     }).toThrow("Id is Required");
   });
 
@@ -14,22 +14,27 @@ describe("Order unit tests", () => {
   });
 
   it("Should calculate total", () => {
-    const item = new OrderItem("i1", "Item 1", 100);
-    const item2 = new OrderItem("i2", "Item 2", 200);
+    const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
     let order = new Order("o1", "c1", [item]);
-
     let total = order.total();
-
-    expect(total).toBe(100);
+    expect(total).toBe(200);
 
     const order2 = new Order("o2", "c2", [item, item2]);
     total = order2.total();
-    expect(total).toBe(300);
+    expect(total).toBe(600);
+  });
+
+  it("Should throw error if the item qte is less or equal zero", () => {
+    expect(() => {
+      const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+      let order = new Order("o1", "c1", [item]);
+    }).toThrow("Quantity must be greater than zero");
   });
 
   it("Should throw error when items is zero", () => {
     expect(() => {
-      let order = new Order("123", "321", []);
+      new Order("123", "321", []);
     }).toThrow("Items are Required");
   });
 });
