14ab29b771819d9c7850750b3f7c0c866f2dc83d
luifean <luisfernandogvv@gmail.com>
2024-08-30 13:25:27 -0300

    tests: new test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -44,4 +44,18 @@ describe("Customer unit tests", () => {
     //Assert
     expect(customer.isActive).toBe(true);
   });
+
+  it("Should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
