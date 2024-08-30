391257e321e5f1d1cb63ce85791ef140dfd5f082
luifean <luisfernandogvv@gmail.com>
2024-08-30 13:19:14 -0300

    feat: new method

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,7 +1,19 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
-
+import OrderItem from "../entity/order_item";
+import { v4 } from "uuid";
 export class OrderService {
-  static total(orderItems: Order[]) {
-    return orderItems.reduce((acc, orderItem) => acc + orderItem.total(), 0);
+  static total(orders: Order[]) {
+    return orders.reduce((acc, order) => acc + order.total(), 0);
+  }
+
+  static placeOrder(customer: Customer, orderItems: OrderItem[]): Order {
+    if (orderItems.length === 0) {
+      throw new Error("Order must have at least one item");
+    }
+    
+    const order = new Order(v4(), customer.id, orderItems);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
   }
 }
